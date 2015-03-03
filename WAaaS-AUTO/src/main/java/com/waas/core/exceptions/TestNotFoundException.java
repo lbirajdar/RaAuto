6b83eaae315ef008c67aca1abea9931c2cbe2e1d
Laxmikant Birajdar <lbirajdar@users.noreply.github.com>
2015-03-03 13:03:40 +0530

    Update TestNotFoundException.java

--- WAaaS-AUTO/src/main/java/com/waas/core/exceptions/TestNotFoundException.java
+++ WAaaS-AUTO/src/main/java/com/waas/core/exceptions/TestNotFoundException.java
@@ -12,16 +12,18 @@ public class TestNotFoundException extends Exception {
     private static final long serialVersionUID = 1L;
 
     public TestNotFoundException(String testName) {
+        
+        super("Testcase name :" + testName + " is either null or invalid");
 
         if (testName == null || testName.trim().equals("")) {
 
-            new Exception("Testcase name cannot be blank");
             System.out.println("Testcase name cannot be blank");
+            
         } else {
 
-            new Exception("Testcase with name : " + testName + " : Not Found");
             System.out.println("Testcase with name : " + testName
                     + " : Not Found");
+                    
         }
 
     }
